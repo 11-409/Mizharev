39344edcd02b65a7b00bfb66afed530425effaa6
spartaknameste <vaceslavmizarev81@gmail.com>
2025-06-11 00:22:47 +0300

    1

---
+++ StackCastom/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace StackCastom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ICharStack stack = new StackCastom(5);
+
+            stack.Add('A');
+            stack.Add('B');
+            stack.Add('C');
+
+            while (!stack.IsEmpty())
+            {
+                Console.WriteLine(stack.Delete());
+            }
+        }
+    }
+}
---
+++ StackCastom/StackCastom.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackCastom
+{
+    public class StackCastom : ICharStack
+    {
+        private char[] storage;
+        private int capacity;
+        private int position;
+
+        public StackCastom(int capacity)
+        {
+            this.capacity = capacity;
+            storage = new char[capacity];
+            position = -1;
+        }
+
+        public bool Add(char value)
+        {
+            if (!IsFull())
+            {
+                storage[++position] = value;
+                return true;
+            }
+            return false;
+        }
+
+        public char Delete()
+        {
+            if (!IsEmpty())
+            {
+                return storage[position--];
+            }
+            return '!';
+        }
+
+        public bool IsEmpty()
+        {
+            return position < 0;
+        }
+
+        public bool IsFull()
+        {
+            return position >= capacity - 1;
+        }
+    }
+}
