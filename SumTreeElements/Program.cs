d2b88c248ab179e737bee81dd5ab0b8c38793813
spartaknameste <vaceslavmizarev81@gmail.com>
2025-06-11 00:32:31 +0300

    1

---
+++ SumTreeElements/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace SumTreeElements
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode head = new TreeNode(1);
+            head.Left = new TreeNode(2);
+            head.Right = new TreeNode(3);
+            head.Left.Left = new TreeNode(4);
+            head.Left.Right = new TreeNode(5);
+
+            Console.WriteLine(TreeTools.GetSumOfElements(head));
+        }
+    }
+
+    class TreeNode
+    {
+        public int Value;
+        public TreeNode Left;
+        public TreeNode Right;
+
+        public TreeNode(int value)
+        {
+            Value = value;
+        }
+    }
+
+    class TreeTools
+    {
+        public static int GetSumOfElements(TreeNode node)
+        {
+            if (node == null) return 0;
+            return node.Value + GetSumOfElements(node.Left) + GetSumOfElements(node.Right);
+        }
+    }
+}
